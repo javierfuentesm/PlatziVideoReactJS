d7b0f14106fe97e599737a7393fc5ac05ccea23c
rjfm <javier.fuentesm@hotmail.com>
2019-10-07 02:10:03 -0500

    Se agrega registro y swith

--- src/routes/App.js
+++ src/routes/App.js
@@ -1,14 +1,17 @@
 import React from 'react';
 
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Home from '../containers/Home';
 import Login from '../containers/Login';
+import Register from '../containers/Register';
 
-const App = () =>( 
+const App = () => (
   <BrowserRouter>
-    <Route exact path='/' component={Home} />
-    <Route exact path='/login' component={Login} />
+    <Switch>
+      <Route exact path='/' component={Home} />
+      <Route exact path='/login' component={Login} />
+      <Route exact path='/register' component={Register} />
+    </Switch>
   </BrowserRouter>
 );
 export default App;
-
