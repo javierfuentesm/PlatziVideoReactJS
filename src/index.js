a0b758e44dfe3d02a19b0be25d202826012c3057
rjfm <javier.fuentesm@hotmail.com>
2019-10-28 07:41:51 -0600

    Ultimos cambios

--- src/index.js
+++ src/index.js
@@ -172,7 +172,7 @@ const initialState = {
 };
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-const store = createStore(reducer, initialState, composeEnhancers);
+const store = createStore(reducer, initialState, composeEnhancers());
 
 ReactDOM.render(
   <Provider store={store}>
