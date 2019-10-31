9eca3dcb75d0cc6f81bbf742365347b20c070d38
rjfm <javier.fuentesm@hotmail.com>
2019-10-30 20:20:32 -0600

    Servir react con express

--- src/server/server.js
+++ src/server/server.js
@@ -1,6 +1,6 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import webpack from 'webpack';
+import express from "express";
+import dotenv from "dotenv";
+import webpack from "webpack";
 
 dotenv.config();
 // eslint-disable-next-line no-unused-vars
@@ -8,11 +8,11 @@ const ENV = process.env.NODE_ENV;
 const PORT = process.env.PORT || 3000;
 const app = express();
 
-if (ENV === 'development') {
-  console.log('Loading dev config');
-  const webpackConfig = require('../../webpack.config');
-  const webpackDevMiddleware = require('webpack-dev-middleware');
-  const webpackHotMiddleware = require('webpack-hot-middleware');
+if (ENV === "development") {
+  console.log("Loading dev config");
+  const webpackConfig = require("../../webpack.config");
+  const webpackDevMiddleware = require("webpack-dev-middleware");
+  const webpackHotMiddleware = require("webpack-hot-middleware");
   const compiler = webpack(webpackConfig);
   const serverConfig = {
     contentBase: `http://localhost:${PORT}`,
@@ -20,18 +20,33 @@ if (ENV === 'development') {
     publicPath: webpackConfig.output.publicPath,
     hot: true,
     historyApiFallback: true,
-    stats: { colors: true },
+    stats: { colors: true }
   };
   app.use(webpackDevMiddleware(compiler, serverConfig));
   app.use(webpackHotMiddleware(compiler));
-
 }
 
-app.get('*', (req, res) => {
-  res.send({ holamundo: true });
+app.get("*", (req, res) => {
+  res.send(`
+  <!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>PlatziVideo</title>
+    <link rel="stylesheet" href="assets/app.css" type="text/css"></link>
+</head>
+<body>
+    <div id="app"></div>
+    <script src="assets/app.js" type="text/javascript"></script>
+    <script src="assets/vendor.js" type="text/javascript"></script>
+    
+</body>
+</html>`);
 });
 
-app.listen(PORT, (err) => {
+app.listen(PORT, err => {
   if (err) console.log(err);
   console.log(`Server running on ${PORT}`);
 });
